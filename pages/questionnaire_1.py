5f8a5fbc4501ba2204d2da85f2cec3e87b4caaad
Ebony59 <fairy3350@163.com>
2023-11-11 17:48:19 +0000

    added divider:

--- pages/questionnaire_1.py
+++ pages/questionnaire_1.py
@@ -11,6 +11,7 @@ chai_style.render_chai_style()
 
 st.title("Question 1 out of 4")
 
+st.divider()
 data = get_sampled_questionnaire_data('data_1')
 
 sorted_items = display_rank_questionnaire(data)
--- pages/questionnaire_2.py
+++ pages/questionnaire_2.py
@@ -11,6 +11,7 @@ chai_style.render_chai_style()
 
 st.title("Question 2 out of 4")
 
+st.divider()
 data = get_sampled_questionnaire_data('data_2')
 
 sorted_items = display_rank_questionnaire(data)
